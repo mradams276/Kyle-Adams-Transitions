571a3f36397571211a13d1385af8c184a9e12e1e
mradams276 <kyle.adams276@gmail.com>
2016-10-03 09:49:56 -0600

    Finished the view controller page structure

--- Kyle Adams Transitions/NewControllerFile.swift
+++ Kyle Adams Transitions/NewControllerFile.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class NewControllerFile : UIViewController
 {
+    @IBOutlet private weak var topImage: UIImageView!
+    
+    @IBOutlet private weak var bottomImage: UIImageView!
+    
     override func viewDidLoad()
     {
         //Load the parent version of a View.
         super.viewDidLoad()
         //Load this pages components.
+        bottomImage.image = UIImage(named: "creep")
+        //This allows me to add a photo to appear after the page loads up.  Image name must be EXACT
     }
 }
