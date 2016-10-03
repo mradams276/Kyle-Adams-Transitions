a783a2063743c99c9c49eb675859aeb69ff5bb72
mradams276 <kyle.adams276@gmail.com>
2016-10-03 07:57:19 -0600

    Linked my new view to the NewControllerFile.

---
+++ Kyle Adams Transitions/NewControllerFile.swift
@@ -0,0 +1,19 @@
+//
+//  NewControllerFile.swift
+//  Kyle Adams Transitions
+//
+//  Created by Adams, Kyle on 10/3/16.
+//  Copyright © 2016 Adams, Kyle. All rights reserved.
+//
+
+import UIKit
+
+class NewControllerFile : UIViewController
+{
+    override func viewDidLoad()
+    {
+        //Load the parent version of a View.
+        super.viewDidLoad()
+        //Load this pages components.
+    }
+}
