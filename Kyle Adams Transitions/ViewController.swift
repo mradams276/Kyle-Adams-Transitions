9155beea66eef76fe42cb0b14ccd9d79d46e80f4
mradams276 <kyle.adams276@gmail.com>
2016-10-03 08:15:25 -0600

    Completed the method call in code

--- Kyle Adams Transitions/ViewController.swift
+++ Kyle Adams Transitions/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func switchScreenToNewControllerFile(sender: UIButton)
+    {
+        performSegueWithIdentifier("toNewFromMain", sender:sender)
+    }
 
 }
 
